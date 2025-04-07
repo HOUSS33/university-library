9670ac4285f65516b7d870977a7cf96a8b8f3ae9
Houssame Tachfine <88225106+HOUSS33@users.noreply.github.com>
2025-04-07 09:29:17 +0100

    Update FileUpload.tsx

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -12,7 +12,7 @@ import { cn } from "@/lib/utils";
 const authenticator = async() => {
     try{
         
-        const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+        const response = await fetch(`/api/auth/imagekit`);
 
         if(!response.ok){
             const errorText = await response.text();
